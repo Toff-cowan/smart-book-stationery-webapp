6865874ac97f59b9a33688efbd8a7e5969f3a4dd
Toff-cowan <cowanchristoff24@gmail.com>
2026-05-14 03:38:18 -0500

    init testing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-
 from app.config import Config
-
-# Extensions
 from app.extensions.db import db
 from app.extensions.jwt import jwt
 from app.extensions.migrate import migrate
@@ -12,33 +9,22 @@ from app.extensions.migrate import migrate
 def create_app():
     app = Flask(__name__)
 
-    # Load Config
     app.config.from_object(Config)
 
-    # Initialize Extensions
+    # Extensions
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
 
-    # Enable CORS
-    CORS(
-        app,
-        resources={r"/api/*": {"origins": "*"}},
-        supports_credentials=True
-    )
+    # CORS
+    CORS(app)
 
-    # =========================
-    # Register Blueprints
-    # =========================
+    # Blueprints
     from app.routes.auth_routes import auth_bp
 
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
-
-    @app.route("/")
-    def home():
-        return {
-            "success": True,
-            "message": "Smart Book & Stationery API Running"
-        }
+    app.register_blueprint(
+        auth_bp,
+        url_prefix="/api/auth"
+    )
 
     return app
